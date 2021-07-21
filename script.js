341e753086d1cd627618db3576d731b001f19342
Leonardo D M Souza <ldms@live.com>
2021-07-21 14:42:00 -0300

    adicionado func para saber o que o usuário digitou

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.querySelector('.busca').addEventListener('submit', (event)=>{
     event.preventDefault();
 
+    let input = document.querySelector('#searchInput').value;
+
+    console.log(input);
+
 });
 
