1b90378f652df467fcc248adf51edd83be1707f0
Leonardo D M Souza <ldms@live.com>
2021-07-21 17:09:45 -0300

    adicionado requisição na api

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-document.querySelector('.busca').addEventListener('submit', (event)=>{
+document.querySelector('.busca').addEventListener('submit', async (event)=>{
     event.preventDefault();
 
     let input = document.querySelector('#searchInput').value;
 
     
     if (input !== ''){
-        showWarning('Carregando...')
-        
+        showWarning('Carregando...');
+
+        let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=677beb8f7a864fc0ef86168da396181a&units=metric&lang=pt_br`;
+
+        let results = await fetch(url);
+        let json = await results.json();
+
+        console.log(json);
     }
 
 });
