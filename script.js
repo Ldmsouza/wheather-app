951db0ff4a646b5f117e07ee7f82cb6641db0556
Leonardo D M Souza <ldms@live.com>
2021-07-21 16:50:45 -0300

    adicionado showWarning

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ document.querySelector('.busca').addEventListener('submit', (event)=>{
 
     let input = document.querySelector('#searchInput').value;
 
-    console.log(input);
+    
+    if (input !== ''){
+        showWarning('Carregando...')
+    }
 
 });
+
+function showWarning(msg){
+    document.querySelector('.aviso').innerHTML = msg;
+}
 
